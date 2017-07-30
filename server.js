a4af1c922678fa88520c35cb089eae1e87c89eb3
Shaunak Kale <epicshaunak@gmail.com>
2017-07-29 17:09:12 -0700

    hi

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is now running on port', app.get('port'));
 });
 
